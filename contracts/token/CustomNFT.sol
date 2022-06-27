77c9581c6f693b2ff977741b19b7160ff73d087e
franyanes <57007339+franyanes@users.noreply.github.com>
2022-06-27 01:21:34 -0300

    deleted extra lines

--- contracts/token/CustomNFT.sol
+++ contracts/token/CustomNFT.sol
@@ -6,8 +6,6 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 string constant TOKEN_NAME = "CustomNFT";
 string constant TOKEN_SYMBOL = "CNFT";
 
-
-
 contract CustomNFT is ERC721(TOKEN_NAME, TOKEN_SYMBOL) {
     function mint(address to, uint256 tokenId) public {
         _mint(to, tokenId);
